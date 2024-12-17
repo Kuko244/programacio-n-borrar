e0c56a1c9f32c71d9e267776c8182f49fc6ef182
Kuko244 <prodriguez2570@ceuandalucia.es>
2024-12-17 16:21:07 +0100

    primera versión proyecto SISISISSISISISI

---
+++ SISISISSISISISI/src/mentira/MeraEdison.java
@@ -0,0 +1,15 @@
+package mentira;
+
+import java.util.Scanner;
+
+public class MeraEdison {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("dime algo");
+		String frase = scanner.next();
+		System.out.println(frase);
+		scanner.close();
+	}
+
+}
